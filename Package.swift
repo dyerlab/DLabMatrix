348967895e9aad46668eac1e292d24b9d64d7c1e
dyerlab <rodney.dyer@gmail.com>
2025-06-20 08:04:54 -0400

    removed all the networks tuff and made it into its own package

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ import PackageDescription
 
 let package = Package(
     name: "DLabMatrix",
-    platforms: [.iOS(.v14), .macOS(.v12)],
+    platforms: [.iOS(.v14), .macOS(.v10_15)],
     products: [
         .library(
             name: "DLabMatrix",
